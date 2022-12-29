cd5d1d8c3a416a429579134355feb5693a2e4fdd
CameronFen <70863320+CameronFen@users.noreply.github.com>
2022-12-29 15:39:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='macrosbi',
+   name='sbiwrapper',
    version='0.1',
    description='The is a wrapper for the sbi library allowing for simulation based estimation of DSGE models',
    author='Cameron Fen',
    author_email='cameronfen@gmail.com',
-   packages=['macrosbi'], 
+   packages=['sbiwrapper'], 
    install_requires=['sbi', 'torch', 'pyknos', 'nflows', 'numpy'], #external packages as dependencies
 )
